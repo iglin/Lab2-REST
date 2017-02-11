9af7988d17df3b57c1f625d0eefc37f2fd0ef830
iglin <iglin@github.com>
2017-02-11 19:58:07 +0400

    NewMeetingActivity interface

--- app/src/main/java/com/iglin/lab2rest/NewMeetingActivity.java
+++ app/src/main/java/com/iglin/lab2rest/NewMeetingActivity.java
@@ -32,14 +32,5 @@ public class NewMeetingActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
